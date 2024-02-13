bbf8ce97fa742521968397f73b4bb68e3679550f
dania0804 <benali.dania@esprit.tn>
2024-02-13 15:07:24 +0100

    Add and remove from cart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 
 const router= createBrowserRouter(
@@ -21,6 +22,8 @@ const router= createBrowserRouter(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route  path='/products/:id' element={<ProductScreen/>} />
+      <Route  path='/cart' element={<CartScreen/>} />
+
     </Route>
   )
 )
