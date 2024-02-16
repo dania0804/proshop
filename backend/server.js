fddfebbc72a42fb8feacc411999e8181d67299b6
dania0804 <benali.dania@esprit.tn>
2024-02-16 12:11:41 +0100

    Auth Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv' 
-dotenv.config();
+import cookieParser from 'cookie-parser';
+
 import products from './data/products.js'
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js';
 
-
+dotenv.config();
 connectDB();
 //const port = 5000;
 const port = process.env.PORT;
@@ -17,6 +18,9 @@ const app = express();
 //BODY parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//cookie parser middleware
+app.use(cookieParser());
+
 
 /* app.get('/', (req, res) => {
      res.send('Api is running'); 
