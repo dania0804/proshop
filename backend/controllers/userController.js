454f3093a97ad94871c3fc3f2358d8bc01b6b98f
dania0804 <benali.dania@esprit.tn>
2024-02-22 15:59:55 +0100

    user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
 
     generateToken(res, user._id);
 
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -46,14 +46,14 @@ const registerUser = asyncHandler(async (req, res) => {
 
     generateToken(res, user._id);
 
-    res.status(201).json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
   });
   }else{
-    res.status(400);
+    res.status(404);
     throw new Error('Invalid user data');
   }
 });
@@ -74,14 +74,46 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('get user profile');
+  const user = await User.findById(req.user.id);
+
+  if(user){
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+  });
+  }else{
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('update user profile');
+  const user = await User.findById(req.user.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Get all users
